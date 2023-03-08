9bf9cfd1466c75c348016ab8de0687ad353ba0df
smck83 <smckellar@gmail.com>
2023-03-08 16:57:31 +1100

    added "sourceLastUpdated"

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ LABEL maintainer="s@mck.la"
 RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     ntp python3 pip wget unzip \
     && mkdir -p /opt/fastapi-siterank/
+# download Top 1 million cisco Umbrella sites
+ADD top-1m.csv main.py run.py /opt/fastapi-siterank/
 RUN wget http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip -P /opt/fastapi-siterank/ && unzip -o /opt/fastapi-siterank/top-1m.csv.zip -d /opt/fastapi-siterank/
-ADD main.py run.py /opt/fastapi-siterank/
 RUN pip3 install fastapi pandas uvicorn["standard"]
 WORKDIR /opt/fastapi-siterank
 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
 from fastapi import FastAPI
 import pandas as pd
-df = pd.read_csv('top-1m.csv', index_col=1, squeeze = True)
+import os
+import json 
+df = pd.read_csv('top-1m.csv', index_col=1, squeeze = True, header=None)
 d = dict(df.to_dict())
 
 app = FastAPI()
 
+sourceage = int(os.path.getmtime('/opt/fastapi-siterank/top-1m.csv'))
 
 
 
@@ -13,8 +16,16 @@ app = FastAPI()
 @app.get("/domain/{domain}")
 async def siteRank(domain):
     try: 
-        return {"domain":domain,"rank": f'{d[domain]:,}'}
+        return {"domain":domain,"rank": f'{d[domain]:,}', "sourceLastUpdated":sourceage }
     except:
 
-        return {"rank": "> 1,000,000","domain":domain}
+        return {"rank": "> 1,000,000","domain":domain, "sourceLastUpdated":sourceage}
 
+@app.get("/top100")
+async def top100():
+    try: 
+        return {"results":df[1:101].to_json(orient='columns') }
+    except:
+        return {"results" : "error"}
+
+#print(df[1:100])
